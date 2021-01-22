8c0f3bde1ab5718086b68381e38b1b792cda12a9
Fikret Gözütok <fikret_gztk@hotmail.com>
2021-01-22 04:58:39 +0300

    Add project files.

---
+++ myDictionary/DictionaryModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace myDictionary
+{
+    class DictionaryModel<K,V>
+    {
+        private List<K> keyArray;
+        private List<V> valueArray;
+        
+
+        public DictionaryModel()
+        {
+            keyArray = new List<K>();
+            valueArray = new List<V>();
+        }
+
+        public void Add(K key, V value)
+        {
+            keyArray.Add(key);
+            valueArray.Add(value);
+        }
+
+        public V Get(K key)
+        {
+            int Ix = keyArray.IndexOf(key);
+            return valueArray[Ix];
+        }
+    }
+}
---
+++ myDictionary/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace myDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DictionaryModel<string, string[]> myDict = new DictionaryModel<string, string[]>();
+
+            myDict.Add("ege", new string[] {"Kütahya", "İzmir", "Muğla"});
+            myDict.Add("marmara", new string[] { "bursa", "istanbul", "Bilecik" });
+            myDict.Add("ıcAnadolu", new string[] { "Ankara", "Konya", "Eskişehir" });
+
+
+            string[] egeArray = myDict.Get("ege");
+            string[] marmaraArray = myDict.Get("marmara");
+            string[] ıcAnadoluArray = myDict.Get("ıcAnadolu");
+
+            getData(egeArray);
+            getData(marmaraArray);
+            getData(ıcAnadoluArray);
+
+            Console.Read();
+        }
+
+        public static void getData(object[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
